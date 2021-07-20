7a53500c96866c710793d8adf990de27082aef20
bdemotte <87666649+bdemotte@users.noreply.github.com>
2021-07-19 23:23:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -118,37 +118,65 @@ st.text('Dongchang Road, Lujiazui Subdistrict, Pudong, 200010, China')
 
 print(SEPERATOR)
 st.header("The Third Largest Building in the World is the Makkah Royal Clock Tower")
+from PIL import Image
+img3 = Image.open("makkah-royal.jpg")
+
+st.image(img3, width=500)
 st.text("The Makkah Royal Clock Tower is located at the following address: ")
 st.text('Abraj Al Bait, Western Haram Square, Al Haram, Mecca, Makkah Al Mukarramah, Makkah Region, 24231, Saudi Arabia')
 
 
 print(SEPERATOR)
 st.header("The Fourth Largest Building in the World is the Ping An Finance Center")
+from PIL import Image
+img4 = Image.open("ping-an.jpg")
+
+st.image(img4, width=500)
 st.text("The Ping An Finance Center is located at the following address: ")
 st.text('Shenzhen Convention & Exhibition Center, 111, Fuhua 3rd Road, Huanggang, Futian Sub-district, Futian District, Shenzhen, Guangdong Province, 518048, China')
 
 print(SEPERATOR)
 st.header("The Fifth Largest Building in theWorld is the Lotte World Tower")
+from PIL import Image
+img5 = Image.open("lotte-tower.jpg")
+
+st.image(img5, width=500)
 st.text("The Lotte World Tower is located at the following address: ")
 st.text('Lotte World Mall, 300, Olympic-ro, Bangi 2(i)-dong, Songpa-gu, Seoul, 05551, South Korea')
 
 
 print(SEPERATOR)
 st.header("The Sixth Largest Building in the World is the One World Trade Center")
+from PIL import Image
+img6 = Image.open("shanghai-tower.jpg")
+
+st.image(img6, width=500)
 st.text("The One World Trade Center is located at the following address: ")
 st.text('One World Trade Center, 285, Fulton Street, Financial District, Manhattan, New York County, New York, 10048, United States')
 
 
 print(SEPERATOR)
 st.header("The Seventh and Eigth Tallest Building in the World are Tied! The Guangzhou CTF Finance Centre and Tianjin CTF Finance Center are both 1,739 ft tall!")
+from PIL import Image
+img7 = Image.open("guang-finance.jpg")
+
+st.image(img7, width=500)
 st.text("The Guangzhou CTF Finance Center is located at the following address: ")
 st.text('IGC 天汇广场, 猎德西浦大街, 猎德街道, Tianhe District, Guangzhou City, Guangdong Province, 510623, China')
+
+from PIL import Image
+img8 = Image.open("tianjin-finance.jpg")
+st.image(img8, width=500)
 st.text("The Tianjin CTF Finance Center is located at the following address: ")
 st.text('Tianjin CTF Finance Centre, 第一大街, Yujiapu Financial District, Binhai New Area, Tianjin, 300457, China')
 
 print(SEPERATOR)
 
 st.header("The Ninth Largest Building in the World is the CITIC Tower")
+from PIL import Image
+img9 = Image.open("citic-tower.jpg")
+
+st.image(img9, width=500)
 st.text("The CITIC Tower is located at the following address: ")
 st.text('Workers’ Gymnasium, West Gongrentiyuchang Road, Dongcheng District, Beijing, 1000001, China')
 
@@ -156,6 +184,10 @@ st.text('Workers’ Gymnasium, West Gongrentiyuchang Road, Dongcheng District, B
 print(SEPERATOR)
 
 st.header("The Tenth Largest Building in the World is the TAIPEI 101")
+from PIL import Image
+img10 = Image.open("taipei-tower.jpg")
+
+st.image(img10, width=500)
 st.text("The TAIPEI 101 is located at the following address: ")
 st.text('Taipei 101, 7, Section 5, Xinyi Road, Xicun Village, Xinyi District, Xinyi Commercial Zone, Taipei, 11049, Taiwan')
 
