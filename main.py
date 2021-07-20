0ea2dd65eb4a0bdc5071b4d6c78d4cc98036e4fa
bdemotte <87666649+bdemotte@users.noreply.github.com>
2021-07-19 22:32:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -102,45 +102,53 @@ st.text('The Burj Khalifa is located at the following address: ')
 from geopy.geocoders import Nominatim
 geolocator = Nominatim(user_agent="4XZD8eEnHEVBSwaEQy4wKIVKZGfkeo5P")
 location1 = geolocator.reverse((df['Latitude'][0], df['Longitude'][0]), language='en')
-st.text(location1.address)
+print(location1.address)
 
 print(SEPERATOR)
 
+st.header("The Second Largest Building in the World is the Shanghai Tower ")
+st.text("The Shanghai Tower is located at the following address: ")
 from geopy.geocoders import Nominatim
 geolocator = Nominatim(user_agent="4XZD8eEnHEVBSwaEQy4wKIVKZGfkeo5P")
 location2 = geolocator.reverse((df['Latitude'][1], df['Longitude'][1]), language='en')
 print(location2.address)
 
 print(SEPERATOR)
-
+st.header("The Third Largest Building in the World is the Makkah Royal Clock Tower")
+st.text("The Makkah Royal Clock Tower is located at the following address: ")
 from geopy.geocoders import Nominatim
 geolocator = Nominatim(user_agent="4XZD8eEnHEVBSwaEQy4wKIVKZGfkeo5P")
 location3 = geolocator.reverse((df['Latitude'][2], df['Longitude'][2]), language='en')
 print(location3.address)
 
 print(SEPERATOR)
-
+st.header("The Fourth Largest Building in the World is the Ping An Finance Center")
+st.text("The Ping An Finance Center is located at the following address: ")
 from geopy.geocoders import Nominatim
 geolocator = Nominatim(user_agent="4XZD8eEnHEVBSwaEQy4wKIVKZGfkeo5P")
 location4 = geolocator.reverse((df['Latitude'][3], df['Longitude'][3]), language='en')
 print(location4.address)
 
 print(SEPERATOR)
-
+st.header("The Fifth Largest Building in theWorld is the Lotte World Tower")
+st.text("The Lotte World Tower is located at the following address: ")
 from geopy.geocoders import Nominatim
 geolocator = Nominatim(user_agent="4XZD8eEnHEVBSwaEQy4wKIVKZGfkeo5P")
 location5 = geolocator.reverse((df['Latitude'][4], df['Longitude'][4]), language='en')
 print(location5.address)
 
 print(SEPERATOR)
-
+st.header("The Sixth Largest Building in the World is the One World Trade Center")
+st.text("The One World Trade Center is located at the following address: ")
 from geopy.geocoders import Nominatim
 geolocator = Nominatim(user_agent="4XZD8eEnHEVBSwaEQy4wKIVKZGfkeo5P")
 location6 = geolocator.reverse((df['Latitude'][5], df['Longitude'][5]), language='en')
 print(location6.address)
 
 print(SEPERATOR)
-
+st.header("The Seventh and Eigth Tallest Building in the World are Tied! The Guangzhou CTF Finance Centre and Tianjin CTF Finance Center are both 1,739 ft tall!")
+st.text("The Guangzhou CTF Finance Center is located at the following address: ")
+st.text("The Tianjin CTF Finance Center is located at the following address: ")
 from geopy.geocoders import Nominatim
 geolocator = Nominatim(user_agent="4XZD8eEnHEVBSwaEQy4wKIVKZGfkeo5P")
 location7 = geolocator.reverse((df['Latitude'][6], df['Longitude'][6]), language='en')
@@ -155,6 +163,8 @@ print(location8.address)
 
 print(SEPERATOR)
 
+st.header("The Ninth Largest Building in the World is the CITIC Tower")
+st.text("The CITIC Tower is located at the following address: ")
 from geopy.geocoders import Nominatim
 geolocator = Nominatim(user_agent="4XZD8eEnHEVBSwaEQy4wKIVKZGfkeo5P")
 location9 = geolocator.reverse((df['Latitude'][8], df['Longitude'][8]), language='en')
@@ -162,8 +172,9 @@ print(location9.address)
 
 print(SEPERATOR)
 
+st.header("The Tenth Largest Building in the World is the TAIPEI 101")
+st.text("The TAIPEI 101 is located at the following address: ")
 from geopy.geocoders import Nominatim
 geolocator = Nominatim(user_agent="4XZD8eEnHEVBSwaEQy4wKIVKZGfkeo5P")
 location10 = geolocator.reverse((df['Latitude'][9], df['Longitude'][9]), language='en')
 print(location10.address)
-
