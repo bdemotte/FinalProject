76da67898cb264ccb236093c8ac6955067b2587b
bdemotte <87666649+bdemotte@users.noreply.github.com>
2021-07-21 13:01:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ SEPERATOR = 500 * '='
 print(SEPERATOR)
 # User Input, map data, and update on the data frame for all locations
 
-ata = pd.DataFrame({
+data = pd.DataFrame({
     'awesome cities' : ['Chicago', 'Minneapolis', 'Louisville', 'Topeka'],
     'lat' : [41.868171, 44.979840,  38.257972, 39.030575],
     'lon' : [-87.667458, -93.272474, -85.765187,  -95.702548]
