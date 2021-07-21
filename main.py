2477273eab715aadea681769a45bea87c296ef07
bdemotte <87666649+bdemotte@users.noreply.github.com>
2021-07-21 13:01:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -45,9 +45,32 @@ SEPERATOR = 500 * '='
 print(SEPERATOR)
 # User Input, map data, and update on the data frame for all locations
 
+ata = pd.DataFrame({
+    'awesome cities' : ['Chicago', 'Minneapolis', 'Louisville', 'Topeka'],
+    'lat' : [41.868171, 44.979840,  38.257972, 39.030575],
+    'lon' : [-87.667458, -93.272474, -85.765187,  -95.702548]
+})
 
-def select_options():
-   st.sidebar.write("Filter On The Map: ")
+# Adding code so we can have map default to the center of the data
+midpoint = (np.average(data['lat']), np.average(data['lon']))
+
+st.deck_gl_chart(
+            viewport={
+                'latitude': midpoint[0],
+                'longitude':  midpoint[1],
+                'zoom': 4
+            },
+            layers=[{
+                'type': 'ScatterplotLayer',
+                'data': data,
+                'radiusScale': 250,
+   'radiusMinPixels': 5,
+                'getFillColor': [248, 24, 148],
+            }]
+        )
+  
+  
+
 
  
 
