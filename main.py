367249b88680f5b54b6424019948b3aab9a3ef4a
bdemotte <87666649+bdemotte@users.noreply.github.com>
2021-07-21 20:26:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def bar_chart():
   cities =sorted(df['City'] == i].shape[0]
   cityList = [x for x in cities]
   for i in cityList:
-    counts.append(df[df['City'] ==i].shape[0])
+    counts.append(df[df['City'] == ].shape[0])
   y_pos = np.arange(len(cityList))
   plt.barh(y_pos, counts)
   plt.yticks(y_pos, cityList)
