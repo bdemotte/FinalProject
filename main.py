bb57ad0f0ea08ee3cb816d298f1e107641b79bee
bdemotte <87666649+bdemotte@users.noreply.github.com>
2021-07-22 11:30:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -56,23 +56,16 @@ def load_data(data):
 def select_options(df):
   st.sidebar.title('Filter On Map')
     
+  CITIES = df['City'].unique()
     
-    
-    
-st.subheader('View Map and Graph?')
-if st.checkbox('Show Map and Graph'):
-    st.write(
-
-    CITIES = df['City'].unique()
-    
-    CITIES_SELECTED = st.sidebar.multiselect('Select Cities to Display', sorted(CITIES))
+  CITIES_SELECTED = st.sidebar.multiselect('Select Cities to Display', sorted(CITIES))
         
-    mark_cities = df['City'].isin(CITIES_SELECTED)
-    df = df[mark_cities]
+  mark_cities = df['City'].isin(CITIES_SELECTED)
+  df = df[mark_cities]
     #df_citiesselected = df[df['City'] == CITIES_SELECTED]
     
    
-    st.map(df)
+  st.map(df)
     
 
         
@@ -105,7 +98,8 @@ def main():
   bar_chart(df)
   
 main()                  
-    )
+
+
   
 # THIS ALL WORKS DO NOT MESS WITH THIS SECTION
 
@@ -215,12 +209,3 @@ st.text('Taipei 101, 7, Section 5, Xinyi Road, Xicun Village, Xinyi District, Xi
 
 
 # MAY NEED TO MAKE CHANGES HERE
-  
- 
-  
-  
-  
-
-
-
-
