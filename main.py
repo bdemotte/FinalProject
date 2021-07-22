b70f808aac302ec9cb3c3c8b96bf5a5b813fdfcc
bdemotte <87666649+bdemotte@users.noreply.github.com>
2021-07-21 21:46:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ Description:    This is a description of the project
 import pandas as pd
 import streamlit as st
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 
 # Renaming the Columns
